16e41bd394b31797305f77430b285c860e437dc9
Samizadeh Mostafa <msamizadeh@ITMCO.COM>
2020-05-31 15:26:58 +0430

    change pcv alarms unit

--- Src/pcv_alarms.c
+++ Src/pcv_alarms.c
@@ -7,8 +7,13 @@ extern int PCV_IPAP,PCV_EPAP, PCV_RISE_TIME, PCV_RATE, PCV_IT_RATIO, PCV_Inspira
 extern int ALARM_PCV_Fr_Maxi,ALARM_PCV_FiO2_Mini,ALARM_PCV_FiO2_Maxi,ALARM_PCV_Vti_Mini,ALARM_PCV_Vti_Maxi,ALARM_PCV_Vte_Mini;
 /// create_response_for_raspberry(111,alarm_code);
 
+float tmp=0;
+
 void PCV_Alarms()
 {
+	
+	 tmp=ALARM_PCV_FiO2_Maxi-(ALARM_PCV_FiO2_Maxi*0.1);
+	
 	 if(PCV_Target_Vt <= ALARM_PCV_Vti_Mini || ALARM_PCV_Vti_Mini>(ALARM_PCV_Vti_Maxi-50))
 		{
 	     create_response_for_raspberry(111,70);
@@ -25,11 +30,11 @@ void PCV_Alarms()
 		{
 			 create_response_for_raspberry(111,73);
 		}
-		else if(ALARM_PCV_FiO2_Mini > (ALARM_PCV_FiO2_Maxi-(ALARM_PCV_FiO2_Maxi*0.1)))
+		else if(ALARM_PCV_FiO2_Mini > ((int)tmp))
 		{
 			 create_response_for_raspberry(111,74);
 		}
-		else if(ALARM_PCV_FiO2_Mini > (ALARM_PCV_FiO2_Maxi-(ALARM_PCV_FiO2_Maxi*0.1)))
+		else if(ALARM_PCV_FiO2_Mini > ((int)tmp))
 		{
 			 create_response_for_raspberry(111,75);
 		}
